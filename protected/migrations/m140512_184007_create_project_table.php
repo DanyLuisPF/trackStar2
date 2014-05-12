730213fc3cd0c019ab76c9248dbbd11d759bc314
camilo reynaga <camilo.reynaga@gmail.com>
2014-05-12 14:49:42 -0500

    Configuraciones iniciales para Primer CRUD project

---
+++ protected/migrations/m140512_184007_create_project_table.php
@@ -0,0 +1,35 @@
+<?php
+
+class m140512_184007_create_project_table extends CDbMigration
+{
+	public function up()
+	{
+            $this->createTable('tbl_project', array(
+                    'id' => 'pk',
+                    'name' => 'string NOT NULL',
+                    'description' => 'text NOT NULL',
+                    'create_time' => 'datetime DEFAULT NULL',
+                    'create_user_id' => 'int(11) DEFAULT NULL',
+                    'update_time' => 'datetime DEFAULT NULL',
+                    'update_user_id' => 'int(11) DEFAULT NULL',
+                    ), 'ENGINE=InnoDB');
+	}
+
+	public function down()
+	{
+            $this->dropTable('tbl_project');
+//		echo "m140512_184007_create_project_table does not support migration down.\n";
+//		return false;
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
